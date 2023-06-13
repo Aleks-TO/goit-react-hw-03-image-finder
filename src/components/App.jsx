2d94e5d4947dd8829e5b3b26a32eb25acd559f5b
Aleks-TO <satov69@gmail.com>
2023-06-13 23:24:53 +0300

    searchbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
-export const App = () => {
-  return (
-    
-  );
-};
+import { Component } from 'react';
+import Searchbar from './searchbar/searchbar';
 
+export class App extends Component {
+  state = {};
 
+  render() {
+    return (
+      <>
+        <Searchbar />
+      </>
+    );
+  }
+}
---
+++ src/components/searchbar/searchbar.jsx
@@ -0,0 +1,24 @@
+import css from './searchbar.module.css';
+
+const Searchbar = () => {
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form}>
+        <button type="submit" className={css.button}>
+          <span className={css.buttonLabel}>Search</span>
+        </button>
+
+        <input
+          className={css.input}
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
+
